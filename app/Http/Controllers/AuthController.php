0a26461badc66639ab637a4ac98ee899336665e1
codewithali <ali-raza@ashlarglobal.com>
2021-06-22 11:30:45 +0500

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Resources\User as UserResource;
 
 class AuthController extends Controller
@@ -27,4 +28,21 @@ class AuthController extends Controller
             ],
         ]);
     }
+    
+    public function login(UserLoginRequest $request) {
+ 
+        if (!$token = auth()->attempt($request->only(['email', 'password']))) {
+         return response()->json([
+            'errors' => [
+                'email' => ['Sorry we cant find you with those details.']
+            ],
+         ],422);
+        }
+ 
+        return (new UserResource($request->user()))->additional([
+            'meta' => [
+             'token' => $token,
+            ],
+        ]);
+     }
 }
